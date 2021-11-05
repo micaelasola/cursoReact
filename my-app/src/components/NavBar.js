02c06e6519a74370610a69f5919a391c3e1b0d19
Micaela Solá <micaelam.sola@gmail.com>
2021-11-04 22:21:20 -0300

    agregando navbar component

---
+++ my-app/src/components/NavBar.js
@@ -0,0 +1,17 @@
+
+const NavBar = () => {
+
+    return (
+    
+    <div>
+        <nav className="navigation">
+            <ul className="home">
+                <li>AlAire</li>
+                <li>Bandas</li>
+                <li>Contacto</li>
+                <li></li>
+            </ul>
+        </nav>
+
+<p> Compra tus tickets aca</p>
+    </div>  default NavBar
